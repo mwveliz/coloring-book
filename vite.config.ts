488a513dfb38a985735dcb35c7b8bf18681f1f9b
mw <mwveliz@gmail.com>
2025-08-21 04:40:46 +0000

    fixed typos

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/coloring-book/', // <-- This line sets the correct base URL for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
-vite.config.ts:1:313: ERROR: Unexpected " "
-    at failureErrorWithLog (/home/runner/work/coloring-book/coloring-book/node_modules/esbuild/lib/main.js:1472:15)
